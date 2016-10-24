a6d769c8c3abdb6661218b70c89fb4860e054a21
nicolasreich <nicolas.reich@epfl.ch>
2016-10-24 19:04:51 +0200

    first map! also, added manually some values and fixed some stuff.

--- 03 - Interactive Viz/helpers.py
+++ 03 - Interactive Viz/helpers.py
@@ -1,10 +1,10 @@
 import googlemaps
+from yandex_translate import YandexTranslate
 """"
 File containing helper methods, factored out to lighten the notebook up
 """
 
 #gmaps = googlemaps.Client('')
-
 def split_and_request(name):
     """
     Most of the university names are of the form 'Long name with many words - LNWMW'.
@@ -15,9 +15,7 @@ def split_and_request(name):
     uni = name.split(' - ')
     for x in uni:
         r = gmaps.places(x)
-        #this isn't right, should be if r['results']
-        #however we probably won't use it anymore so might as well leave it
-        if r:
+        if(r['status'] != 'ZERO_RESULTS'):
             return r
     #no result        
     print(name)
@@ -44,6 +42,22 @@ def extract_lat_lng(row):
     else:
         return row['Raw localisation']['results'][0]['geometry']['location']
 
-def print_index_if_empty(row):
-    if not row['Raw localisation']['results']:
-        print(row.name)
+def get_canton(location):
+    if location is None:
+        return None
+    if next(x for x in location[0]['address_components'] if 'country' in x['types'])['short_name'] == 'CH':
+        return next(x for x in location[0]['address_components'] if 'administrative_area_level_1' in x['types'])['short_name']
+    return None
+
+
+def split_and_geocode(name):
+    uni = name.split(' - ')
+    for x in uni:
+        response = gmaps.geocode(x, region='ch')
+        if(response):
+            return response
+    return None
+
+def set_coordinates_and_canton(df, index, lng, lat, canton):
+    df.set_value(index, 'Coordinates', {'lng': lng, 'lat': lat})
+    df.set_value(index, 'Canton', canton)
